c2839aecad580ba9e68b6ddc290897ef2cd0eca8
wujingquan <i@wujingquan.com>
2024-01-12 22:56:27 +0800

    fix: 登录状态

--- src/common/decorators/user.decorator.ts
+++ src/common/decorators/user.decorator.ts
@@ -3,10 +3,7 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 export const User = createParamDecorator(
   (data: string, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
-    // const user = request.user;
-    const user = {
-      id: 1,
-    };
+    const user = request.adminUser;
 
     return data ? user?.[data] : user;
   },
